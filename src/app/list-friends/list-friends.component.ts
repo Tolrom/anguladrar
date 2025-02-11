2af9ba0c2595cde241da85e561157a1af74a868a
tolrom <max.le.morlot@gmail.com>
2025-02-11 13:53:40 +0100

    ngif

--- src/app/list-friends/list-friends.component.ts
+++ src/app/list-friends/list-friends.component.ts
@@ -1,21 +1,26 @@
 import { Component } from '@angular/core';
 import { OneFriendComponent } from '../one-friend/one-friend.component';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-list-friends',
-  imports: [OneFriendComponent],
+  imports: [OneFriendComponent, NgIf],
   templateUrl: './list-friends.component.html',
   styleUrl: './list-friends.component.css'
 })
 export class ListFriendsComponent {
 
-  bind: string = "";
+  bind: string = "Elon Musk";
   onUpdateFriendsList(eventInput:Event): void {
     this.bind = (<HTMLInputElement>eventInput.target).value ;
   }
 
-  claques: number = 0;
-  onClaque(): void{
-    this.claques++;
+  target = {
+    name: "",
+    dead: false
+  }
+  onSup(): void{
+    this.target.dead = true;
+    this.target.name = this.bind;
   }
 }
